8d7ecf2f7b5e68daa2c46ffea6cab35726043db2
Ahmed Khaled <ahmed@mrkhalednasr.com>
2024-03-13 16:02:09 +0200

    movie details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import AboutUs from "./pages/AboutUs";
 import OurWork from "./pages/OurWork";
 import ContactUs from "./pages/ContactUs";
+import MovieDetail from "./components/MovieDetail";
 import GlobalStyle from "./GlobalStyle";
 import Nav from "./components/Nav";
 import { Routes, Route } from "react-router-dom";
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/capture/" element={<AboutUs />} />
         <Route path="/capture/work" element={<OurWork />} />
+        <Route path="/capture/work/:title" element={<MovieDetail />} />
         <Route path="/capture/contact" element={<ContactUs />} />
       </Routes>
     </div>
