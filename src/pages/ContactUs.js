20c9fb64677f0f8f1744ceae15495bf3a2e628a0
Ahmed Khaled <ahmed@mrkhalednasr.com>
2024-03-14 16:34:46 +0200

    contact us

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,12 +1,73 @@
+import React from "react";
+//Animations
 import { motion } from "framer-motion";
-import { pageAnim } from "../Animations";
+import { pageAnim, titleAnim } from "../Animations";
+import styled from "styled-components";
 
 const ContactUs = () => {
   return (
-    <motion.div variants={pageAnim} initial="hidden" animate="show" exit="exit">
-      <h1>Contact Us</h1>
-    </motion.div>
+    <ContactStyle exit="exit" variants={pageAnim} initial="hidden" animate="show">
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+        </Hide>
+      </Title>
+      <div>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Send Us A Message</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Send an email.</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Social Media</h2>
+          </Social>
+        </Hide>
+      </div>
+    </ContactStyle>
   );
 };
+
+const ContactStyle = styled(motion.div)`
+  padding: 2.5rem 5rem;
+  color: white;
+  min-height: 90vh;
+  @media (max-width: 1500px) {
+    padding: 2rem;
+    font-size: 1rem;
+  }
+`;
+const Title = styled.div`
+  font-weight: bold;
+  margin-bottom: 4rem;
+  color: #23d997;
+  @media (max-width: 1500px) {
+    margin-top: 5rem;
+  }
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: white;
+`;
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+`;
 
 export default ContactUs;
