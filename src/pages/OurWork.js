ecd54100fa13da7ad01722208609beb08b2e6760
Ahmed Khaled <ahmed@mrkhalednasr.com>
2024-03-14 13:45:23 +0200

    our work animtion page

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -4,29 +4,41 @@ import athlete from "../img/athlete-small.png";
 import theracer from "../img/theracer-small.png";
 import goodtimes from "../img/goodtimes-small.png";
 import { motion } from "framer-motion";
-import { pageAnim } from "../Animations";
+import { pageAnim, fade, photoAnim, lineAnim, slider, sliderContainer } from "../Animations";
 const OurWork = () => {
   return (
     <Work variants={pageAnim} initial="hidden" animate="show" exit="exit">
+      <motion.div variants={sliderContainer}>
+        <Frame1 variants={slider}></Frame1>
+        <Frame2 variants={slider}></Frame2>
+        <Frame3 variants={slider}></Frame3>
+        <Frame4 variants={slider}></Frame4>
+      </motion.div>
       <Movie>
-        <h2>The Athelete</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>The Athelete</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/capture/work/the-athlete">
-          <img src={athlete} alt="athlete" />
+          <Hide>
+            <motion.img variants={photoAnim} src={athlete} alt="athlete" />
+          </Hide>
         </Link>
       </Movie>
       <Movie>
-        <h2>The Racer</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>The Racer</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/capture/work/the-racer">
-          <img src={theracer} alt="racer" />
+          <Hide>
+            <motion.img variants={photoAnim} src={theracer} alt="racer" />
+          </Hide>
         </Link>
       </Movie>
       <Movie>
-        <h2>Good Times</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>Good Times</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/capture/work/good-times">
-          <img src={goodtimes} alt="goodtimes" />
+          <Hide>
+            <motion.img variants={photoAnim} src={goodtimes} alt="goodtimes" />
+          </Hide>
         </Link>
       </Movie>
     </Work>
@@ -39,6 +51,8 @@ const Work = styled(motion.div)`
   padding: 2.5rem 5rem;
   h2 {
     padding: 1rem 0rem;
+    font-weight: bold;
+    color: #23d997;
   }
 `;
 const Movie = styled.div`
@@ -55,4 +69,25 @@ const Movie = styled.div`
   }
 `;
 
+const Hide = styled.div`
+  overflow: hidden;
+`;
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
+`;
 export default OurWork;
