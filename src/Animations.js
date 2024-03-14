7ffa3f66aab5a5de8e17d455bb512f1fbefecdd9
Ahmed Khaled <ahmed@mrkhalednasr.com>
2024-03-14 12:43:38 +0200

    about us section animations

--- src/Animations.js
+++ src/Animations.js
@@ -8,6 +8,8 @@ export const pageAnim = {
     y: 0,
     transition: {
       duration: 1,
+      when: "beforeChildren",
+      staggerChildren: 0.25,
     },
   },
   exit: {
@@ -17,4 +19,29 @@ export const pageAnim = {
       duration: 1,
     },
   },
+};
+
+export const titleAnim = {
+  hidden: {
+    y: 300,
+  },
+  show: {
+    y: 0,
+    transition: { duration: 0.75, ease: "easeOut" },
+  },
+};
+
+export const fade = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: { duration: 0.75, ease: "easeOut" },
+  },
+};
+
+export const photoAnim = {
+  hidden: { scale: 1.5, opacity: 0 },
+  show: { scale: 1, opacity: 1, transition: { duration: 0.75, ease: "easeOut" } },
 };
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,28 +1,34 @@
 import home1 from "../img/home1.png";
 import { Container, Description, Image, Hide } from "../Styles";
+import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../Animations";
+import Wave from "./Wave";
 const AboutSection = () => {
   return (
     <Container>
       <Description>
         <div className="title">
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span>
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>come true</h2>
+            <motion.h2 variants={titleAnim}>come true</motion.h2>
           </Hide>
         </div>
-        <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills</p>
-        <button>Contact Us</button>
+        <motion.p variants={fade}>
+          Contact us for any photography or videography ideas that you have. We have professionals with amazing skills
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
+      <Wave />
     </Container>
   );
 };
